9f489e3fd6923622b9e3fa8eac50532cbbda2684
Anandrajoriya <anandrajoriya03@gmail.com>
2024-12-13 22:33:00 +0530

    i phone website

---
+++ src/components/videoCarousel.jsx
@@ -0,0 +1,5 @@
+function videoCarousel() {
+  return <div>videoCarousel</div>;
+}
+
+export default videoCarousel;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        blue:'#3997ff',
+        gray:{
+          default:'#86868b',
+          100:'#94928b',
+          200:'#afafaf',
+          300:'#42424570'
+        },
+        zinc:'#101010'
+      }
+    },
+  },
+  plugins: [],
+}
+
